cb521c46e0cd30ba8d8db66bc0e0fd264ebbb65b
myeongju00 <j51677767@gmail.com>
2022-02-21 18:54:18 +0900

    [Silver IV] Title: 개미, Time: 76 ms, Memory: 11608 KB -BaekjoonHub

---
+++ 백준/Silver/10158.개미/개미.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int w = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+
+        int t = Integer.parseInt(br.readLine());
+
+        x = w - Math.abs(w - (x + t) % (2 * w));
+        y = h - Math.abs(h - (y + t) % (2 * h));
+
+        sb.append(x).append(" ").append(y);
+        System.out.println(sb);
+    }
+}
