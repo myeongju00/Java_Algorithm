8921a1c70faa60bdc9b9acabd5efbc3ba45397b1
myeongju00 <j51677767@gmail.com>
2022-03-06 19:51:44 +0900

    [Silver IV] Title: 돌 게임 2, Time: 72 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        if (N % 2 == 0) {
+            System.out.println("SK");
+        } else {
+            System.out.println("CY");
+        }
+
+    }
+}
