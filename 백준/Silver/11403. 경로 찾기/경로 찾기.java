99a746b3494304de3d92184d26a80aa42bb85c7d
myeongju00 <j51677767@gmail.com>
2022-03-22 22:22:18 +0900

    [Silver I] Title: 경로 찾기, Time: 308 ms, Memory: 18188 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    static int N;
+    static int[][] board;
+
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        board = new int[N][N];
+
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                if(board[i][j] == 1) dfs(i, j);
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void dfs(int x, int y) {
+        board[x][y] = 1;
+        for(int i = 0; i < N; i++) {
+            if(board[y][i] == 1 && board[x][i] != 1) dfs(x, i);
+        }
+    }
+
+}
