a009ead7e80045ad64d6bfc8affbf0d9d0b3df5f
myeongju00 <j51677767@gmail.com>
2022-04-06 01:35:03 +0900

    [Silver II] Title: 회의실 배정, Time: 464 ms, Memory: 41992 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -16,11 +17,14 @@ public class Main {
             time[i][1] = Integer.parseInt(st.nextToken()); // 종료
         }
 
-        Arrays.sort(time, (o1, o2) -> {
-            if(o1[1] == o2[1]) { //종료시간이 같을 경우 시작 시간이 빠른 순
-                return o1[0] - o2[0];
+        Arrays.sort(time, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[1] == o2[1]) { //종료시간이 같을 경우 시작 시간이 빠른 순
+                    return o1[0] - o2[0];
+                }
+                return o1[1] - o2[1];
             }
-            return o1[1] - o2[1];
         });
 
         int cnt = 0;
