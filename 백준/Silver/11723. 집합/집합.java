adbef50222b5db98b7be692960eb24c3815f0c31
myeongju00 <j51677767@gmail.com>
2022-04-07 13:58:13 +0900

    [Silver V] Title: 집합, Time: 992 ms, Memory: 324392 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -10,32 +10,17 @@ public class Main {
         int S = 0;
         int M = Integer.parseInt(br.readLine());
         StringTokenizer st;
-        for (int i = 0; i < M; i++) {
+        while (M-- > 0) {
             st = new StringTokenizer(br.readLine());
             String str = st.nextToken();
-            switch (str) {
-                case "all" :
-                    S = (1 << 21) - 1;
-                    break;
-                case "empty" :
-                    S = 0;
-                    break;
-                default:
-                    int num = Integer.parseInt(st.nextToken());
-                    switch (str) {
-                        case "add" :
-                            S |= (1 << num);
-                            break;
-                        case "remove" :
-                            S &= ~(1 << num);
-                            break;
-                        case "check" :
-                            sb.append((S & (1 << num)) != 0 ? 1 : 0).append("\n");
-                            break;
-                        case "toggle" :
-                            S ^= (1 << num);
-                            break;
-                    }
+            if(str.equals("all")) S = (1 << 21) - 1;
+            else if(str.equals("empty")) S = 0;
+            else {
+                int num = Integer.parseInt(st.nextToken());
+                if(str.equals("add")) S |= (1 << num);
+                else if(str.equals("remove")) S &= ~(1 << num);
+                else if(str.equals("check")) sb.append((S & (1 << num)) != 0 ? 1 : 0).append("\n");
+                else if(str.equals("toggle")) S ^= (1 << num);
             }
         }
         System.out.println(sb);
