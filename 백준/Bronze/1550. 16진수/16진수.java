5bfb9cfa6ca7b81cca54f971ac513f9377e96058
myeongju00 <j51677767@gmail.com>
2022-04-17 20:29:19 +0900

    [Bronze V] Title: 16진수, Time: 100 ms, Memory: 12808 KB -BaekjoonHub

---
+++ 백준/Bronze/1550. 16진수/16진수.java
@@ -0,0 +1,9 @@
+import java.util.*;
+ 
+class Main {
+  public static void main(String[] args) throws Exception {
+      Scanner sc = new Scanner(System.in);
+      String a = sc.next();
+      System.out.println(Integer.parseInt(a,16));
+      }
+}
