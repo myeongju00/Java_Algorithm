1756c600eba08776aad6e67b901584361ca4407b
myeongju00 <j51677767@gmail.com>
2022-04-28 22:29:02 +0900

    [Bronze II] Title: 농구 경기, Time: 80 ms, Memory: 11628 KB -BaekjoonHub

---
+++ 백준/Bronze/1159. 농구 경기/농구 경기.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		char[] arr = new char[N];
+		String result = "";
+		
+		for(int i = 0; i < N; i++) {
+			arr[i] = (br.readLine()).charAt(0);
+		}
+		
+		Arrays.sort(arr);
+		
+		for(int i = 0; i < (N - 1); i++) {
+			int count = 0;
+			for(int j = (i + 1); j < N; j++) {
+				if(arr[i] == arr[j] && arr[i] != '0') {
+					count++;
+					arr[j] = '0';
+				}
+			}
+			if(count > 3) {
+				result += arr[i];
+			}
+		}
+		if(result.equals("")) {
+			System.out.println("PREDAJA");
+		}else {
+			System.out.println(result);
+		}
+	}
+}
