f7f5c4347b39ee24653c1855f94fc063b2159bfb
myeongju00 <j51677767@gmail.com>
2022-06-25 17:02:50 +0900

    [Bronze II] Title: 숫자 놀이, Time: 80 ms, Memory: 11496 KB -BaekjoonHub

--- 백준/Bronze/2145. 숫자 놀이/숫자 놀이.java
+++ 백준/Bronze/2145. 숫자 놀이/숫자 놀이.java
@@ -1,30 +1,37 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.lang.*;
+import java.io.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+class Main {
+
+    public static void main (String[] args) throws java.lang.Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         while (true) {
+            // 입력 받기
             String tmp = br.readLine();
+
+            // 종료 조건
             if (tmp.equals("0")) {
                 break;
             }
+
             String[] num = tmp.split("");
+            // 계산 하기
             testNum(num);
         }
     }
-
+    
     static void testNum(String[] num) {
-        int rslt = 0;
-        for (int i = 0; i < num.length; i++) {
-            rslt += Integer.parseInt(num[i]);
+        int result = 0;
+        for (String s : num) {
+            result += Integer.parseInt(s);
         }
-        if((rslt / 10) == 0) {
-            System.out.println(rslt);
+        
+        if((result / 10) == 0) {
+            // 결과가 나왔으므로 출력
+            System.out.println(result);
         } else {
-            testNum(String.valueOf(rslt).split(""));
+            testNum(String.valueOf(result).split(""));
         }
     }
+
 }
