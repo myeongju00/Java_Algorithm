ac4982dbfece9e0c973f13ab0a8f52698cbf1b86
myeongju00 <j51677767@gmail.com>
2022-07-16 13:57:32 +0900

    [Bronze V] Title: 카드 게임, Time: 80 ms, Memory: 11492 KB -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		ArrayList<Integer> numbers = new ArrayList<Integer>();
+		for(int i = 0; i < 5; i++) {
+			numbers.add(Integer.parseInt(bf.readLine()));
+		}
+		
+		int result = 0;
+		for (int i : numbers) {
+			result += i;
+		}
+		System.out.println(result);
+
+	}
+
+}
