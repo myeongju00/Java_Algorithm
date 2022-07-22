c4cf5c61bed41e2ba3aaedd076af8d38523fa75d
myeongju00 <j51677767@gmail.com>
2022-07-23 02:26:45 +0900

    [Gold III] Title: 파티, Time: 172 ms, Memory: 17412 KB -BaekjoonHub

---
+++ 백준/Gold/1238. 파티/파티.java
@@ -0,0 +1,98 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, X;
+    static final int INF = 987654321;
+    static ArrayList<ArrayList<Node>> arrList, reverse_arrList;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        X = Integer.parseInt(st.nextToken());
+
+        arrList = new ArrayList<>(); // 문제의 입력을 그대로 받은 배열
+        reverse_arrList = new ArrayList<>(); // 문제의 입력을 반대로 받은 배열
+
+        for (int i = 0; i <= N; i++) {
+            arrList.add(new ArrayList<>());
+            reverse_arrList.add(new ArrayList<>());
+        }
+
+        // arrList와 reverse_arrList를 각각 단방향 인접리스트로 구현
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+
+            arrList.get(start).add(new Node(end, weight));
+            reverse_arrList.get(end).add(new Node(start, weight));
+        }
+
+        int[] dist1 = dijkstra(arrList); // X에서 시작점들 사이의 최단거리
+        int[] dist2 = dijkstra(reverse_arrList); // 시작점들에서 X 사이의 최단거리
+
+        int ans = 0;
+        for (int i = 1; i <= N; i++) {
+            ans = Math.max(ans, dist1[i] + dist2[i]);
+        }
+
+        System.out.println(ans);
+    }
+
+    // 다익스트라 알고리즘
+    public static int[] dijkstra(ArrayList<ArrayList<Node>> a) {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.offer(new Node(X, 0));
+
+        boolean[] check = new boolean[N + 1];
+        int[] dist = new int[N + 1];
+        Arrays.fill(dist, INF);
+        dist[X] = 0;
+
+        while (!pq.isEmpty()) {
+            Node curTown = pq.poll();
+            int cur = curTown.end;
+
+            if (!check[cur]) {
+                check[cur] = true;
+
+                for (Node town : a.get(cur)) {
+                    if (!check[town.end] && dist[town.end] > dist[cur] + town.weight) {
+                        dist[town.end] = dist[cur] + town.weight;
+                        pq.add(new Node(town.end, dist[town.end]));
+                    }
+                }
+            }
+        }
+        return dist;
+    }
+
+}
+
+class Node implements Comparable<Node> {
+    int end;
+    int weight;
+
+    public Node(int end, int weight) {
+        this.end = end;
+        this.weight = weight;
+    }
+
+
+    @Override
+    public int compareTo(Node o) {
+        return weight - o.weight;
+    }
+}
