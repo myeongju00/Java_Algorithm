1c0657311d14d60ae12033b593e7ec552a45b5f3
myeongju00 <j51677767@gmail.com>
2022-11-23 00:40:42 +0900

    [Bronze III] Title: 알람 시계, Time: 76 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+ 
+public class Main {
+	public static void main(String[] args) throws IOException {
+        
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		int H = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		StringBuilder sb = new StringBuilder();
+ 
+		if(M < 45) {
+			if(H == 0){
+				H = 23;
+				sb.append(H).append(' ');
+			} else {
+				H--;
+				sb.append(H).append(' ');
+			}
+			sb.append(M = 60 - (45 - M));
+		}
+		else {
+			sb.append(H).append(' ').append(M - 45);
+		}
+ 
+		System.out.println(sb);
+	}
+}
