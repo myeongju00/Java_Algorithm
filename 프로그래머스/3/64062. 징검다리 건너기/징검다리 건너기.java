4269d220e8e9897a06bb0413f3a5971c282fcb95
myeongju00 <j51677767@gmail.com>
2023-11-24 20:06:09 +0900

    [level 3] Title: 징검다리 건너기, Time: 19.07 ms, Memory: 61.5 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -16,13 +16,15 @@ class Solution {
     }
     
     public boolean check(int mid, int k, int[] stones) {
-        int count = 0;
-        for(int i = 0; i < stones.length; i++) {
-            if(stones[i] < mid) { //mid보다 작으면 mid가 건널 수 없다.
-                count++;
-                if(count >= k) return false;
-            } else count = 0;
-        }
-        return true;
+    int count = 0;
+    for (int stone : stones) {
+        if (stone < mid) { //mid보다 작으면 mid가 건널 수 없다.
+            count++;
+            if (count >= k)
+                return false;
+        } else
+            count = 0;
+    }
+    return true;
     }
 }
