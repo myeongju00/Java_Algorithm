7f58b8fef90d7083cc6d3019bfe18f7e87ab8475
myeongju00 <j51677767@gmail.com>
2024-01-02 01:12:37 +0900

    [Silver IV] Title: 듣보잡, Time: 236 ms, Memory: 25724 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -4,35 +4,41 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.StringTokenizer;
 
+
 public class Main {
+    static StringBuilder sb = new StringBuilder();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        Set<String> set = new HashSet<>();
 
-        HashSet<String> set = new HashSet<>();
-        for (int i = 0; i < n; i++) {
-            set.add(br.readLine());
+
+        for(int i = 0; i < N; i++) {
+            String name = br.readLine();
+            set.add(name);
         }
-
-        ArrayList<String> result = new ArrayList<>();
-        for (int i = 0; i < m; i++) {
-            String tmp = br.readLine();
-            if(set.contains(tmp)){
-                result.add(tmp);
+        List<String> answerList = new ArrayList<>();
+        for (int i = N + 2; i < N + M + 2; i++) {
+            String name = br.readLine();
+            if(set.contains(name)) {
+                answerList.add(name);
             }
         }
 
-        Collections.sort(result);
+        Collections.sort(answerList);
 
-        sb.append(result.size()).append("\n");
-        for (String s : result) {
-            sb.append(s).append("\n");
+        sb.append(answerList.size()).append("\n");
+        for (String name : answerList) {
+            sb.append(name).append("\n");
         }
+
         System.out.println(sb);
     }
 }
