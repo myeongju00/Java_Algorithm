395fba574bc2ea1dbbbad0b7255f06431f1794de
myeongju00 <j51677767@gmail.com>
2024-01-30 00:04:04 +0900

    [Silver II] Title: 스택 수열, Time: 296 ms, Memory: 28832 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,16 +1,23 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.Stack;
+import java.util.StringTokenizer;
+
 
 public class Main {
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
-
         Stack<Integer> stack = new Stack<>();
+        boolean resultFlag = true;
         int startNum = 0;
 
         while (N-- > 0) {
@@ -22,13 +29,21 @@ public class Main {
                 }
                 startNum = number;
             } else if (stack.peek() != number) {
-                System.out.println("NO");
-                return;
+                resultFlag = false;
+                break;
             }
 
             stack.pop();
             sb.append('-').append('\n');
         }
-        System.out.println(sb);
+
+        if (!resultFlag) {
+            sb = new StringBuilder();
+            sb.append("NO");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
     }
 }
