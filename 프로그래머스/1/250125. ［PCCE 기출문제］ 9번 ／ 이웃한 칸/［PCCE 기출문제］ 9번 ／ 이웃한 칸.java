2b71a082ffe50d197dea3eace1e6a5bb837b82a3
myeongju00 <j51677767@gmail.com>
2024-03-20 20:24:15 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 이웃한 칸, Time: 0.02 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.java
+++ 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.java
@@ -1,26 +1,22 @@
 class Solution {
 
     String[][] board;
-    boolean[][] visited;
     int[] dh = {1, 0, 0, -1};
     int[] dw = {0, 1, -1, 0};
     int count = 0;
     public int solution(String[][] board, int h, int w) {
         this.board = board;
-        this.visited = new boolean[board.length][board[0].length];
         dfs(h, w);
         return count;
     }
 
     private void dfs (int h, int w) {
-        visited[h][w] = true;
-
         for (int i = 0; i < 4; i++) {
             int nh = h + dh[i];
             int nw = w + dw[i];
 
             if (nh < 0 || nh >= board.length ||
-                nw < 0 || nw >= board[0].length || visited[nh][nw]) {
+                nw < 0 || nw >= board[0].length) {
                 continue;
             }
             if (board[nh][nw].equals(board[h][w])) {
