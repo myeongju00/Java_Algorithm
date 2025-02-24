b3aa94a430bb6e91410a6d7114789f16ce64c3b7
myeongju00 <j51677767@gmail.com>
2025-02-24 15:51:30 +0700

    [Silver II] Title: 유기농 배추, Time: 100 ms, Memory: 13352 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,62 +1,62 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.StringTokenizer;
 
 
 public class Main {
-    static int[] dx = { 1, -1, 0, 0};
-    static int[] dy = { 0, 0, 1, -1};
-    static int[][] map;
+    static int[][] board;
     static boolean[][] visited;
-    static int N, M, count = 0;
+
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
+    static int count, N, M;
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int T = Integer.parseInt(br.readLine());
-        while (T-- > 0) {
+        int t = Integer.parseInt(br.readLine());
+        for(int n = 0; n  < t; n++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             M = Integer.parseInt(st.nextToken());
             N = Integer.parseInt(st.nextToken());
             int K = Integer.parseInt(st.nextToken());
 
             count = 0;
-            map = new int[M][N];
+            board = new int[M][N];
             visited = new boolean[M][N];
 
-            for (int i = 0; i < K; i++) {
+            for(int i = 0; i < K; i++) {
                 st = new StringTokenizer(br.readLine());
-                int X = Integer.parseInt(st.nextToken());
-                int Y = Integer.parseInt(st.nextToken());
-                map[X][Y] = 1;
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+                board[x][y] = 1;
             }
 
-            for (int i = 0; i < M; i++) {
-                for (int j = 0; j < N; j++) {
-                    if(map[i][j] == 1 && !visited[i][j]) {
+            for(int i = 0; i < M; i++) {
+                for(int j = 0; j < N; j++) {
+                    if(!visited[i][j] && board[i][j] == 1) {
                         dfs(i, j);
                         count++;
                     }
                 }
             }
 
-            sb.append(count).append("\n");
+            System.out.println(count);
         }
-        System.out.println(sb);
     }
 
     public static void dfs(int x, int y) {
         visited[x][y] = true;
-        for (int k = 0; k < 4; k++) {
-            int nx = x + dx[k];
-            int ny = y + dy[k];
 
-            if(nx < 0 || ny < 0 || nx >= M || ny >= N) {
+        for(int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if(nx > M - 1 || nx < 0 || ny > N - 1 || ny < 0) {
                 continue;
             }
-
-            if(!visited[nx][ny] && map[nx][ny] == 1) {
+            if(!visited[nx][ny] && board[nx][ny] == 1) {
                 dfs(nx, ny);
             }
         }
