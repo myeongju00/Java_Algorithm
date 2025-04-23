18c510822d0c497bb614aa285bce834b584184f4
myeongju00 <j51677767@gmail.com>
2025-04-23 23:52:09 +0700

    [level 3] Title: 베스트앨범, Time: 4.38 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,63 +1,43 @@
 import java.util.*;
+class Solution {
+    public int[] solution(String[] genres, int[] plays) {
+        Map<String, Integer> genresMap = new HashMap<>();
+        Map<String, List<Song>> songMap = new HashMap<>();
 
-public class Solution {
-
-    static class Music{
-        String genre;
-        int play;
-        int idx;
-
-        public Music(String genre, int play, int idx) {
-            this.genre = genre;
-            this.play = play;
-            this.idx = idx;
-        }
-    }
-
-    public static int[] solution(String[] genres, int[] plays) {
-
-        HashMap<String, Integer> map = new HashMap<>();
-        for(int i=0; i<genres.length; i++){
-            map.put(genres[i], map.getOrDefault(genres[i], 0)+plays[i]);
-        }
-
-        // 1. 장르 선정
-        ArrayList<String> genres_ordered = new ArrayList<>();
-        while(map.size()!=0){
-            int max = -1;
-            String max_key = "";
-            for(String key : map.keySet()){
-                int tmp_cnt = map.get(key);
-                if(tmp_cnt>max){
-                    max = tmp_cnt;
-                    max_key = key;
-                }
-            }
-            genres_ordered.add(max_key);
-            map.remove(max_key);
-        }
-		
-        // 2. 장르 내 노래 선정
-        ArrayList<Music> result = new ArrayList<>();
-        for(String gern : genres_ordered){
-            ArrayList<Music> list = new ArrayList<>();
-            for(int i=0; i<genres.length; i++){
-                if(genres[i].equals(gern)){
-                    list.add(new Music(gern, plays[i], i));
-                }
-            }
-            Collections.sort(list, (o1, o2) -> o2.play - o1.play); // 내림차순 소팅
-            result.add(list.get(0)); 	// 1개는 무조건 수록
-            if(list.size()!=1){ 	// 더 수록할 곡이 있으면(==장르 내의 노래가 1개보다 많으면) 수록
-                result.add(list.get(1));
-            }
+        
+        for(int i = 0; i < genres.length; i++) {
+            genresMap.put(genres[i], genresMap.getOrDefault(genres[i], 0) + plays[i]);
+            songMap.computeIfAbsent(genres[i], k -> new ArrayList<>()).add(new Song(i, plays[i]));
         }
         
-        // print result
-        int[] answer = new int[result.size()];
-        for(int i=0; i<result.size(); i++){
-            answer[i] = result.get(i).idx;
+        List<Integer> result = new ArrayList<>();
+        //리스트로 변환 후 정렬 필요
+        List<String> sortedGenres = new ArrayList<>(genresMap.keySet());
+        sortedGenres.sort((a, b) -> genresMap.get(b) - genresMap.get(a));
+        
+        for(String genre : sortedGenres) {
+            List<Song> list = songMap.get(genre);
+            list.sort((a, b) -> {
+                if(b.plays == a.plays) {
+                    return a.id - b.id;
+                }
+                return b.plays - a.plays;
+            });
+            for (int i = 0; i < Math.min(2, list.size()); i++) {
+                result.add(list.get(i).id);
+            }
+                
         }
-        return answer;
+        return result.stream().mapToInt(i -> i).toArray();
+    }
+    
+    public class Song {
+        int id;
+        int plays;
+
+        public Song(int id, int plays) {
+            this.id = id;
+            this.plays = plays;
+        }
     }
 }
